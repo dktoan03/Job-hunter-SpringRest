180b5089f2e518070d1ee6656520e373043a64d7
Toan Duong <dktoan03@gmail.com>
2024-11-10 23:20:58 +0700

    add company model, use lombok(get,set) & format ISO

---
+++ src/main/java/vn/dkt/jobhunter/config/DateTimeFormatConfiguration.java
@@ -0,0 +1,16 @@
+package vn.dkt.jobhunter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DateTimeFormatConfiguration implements WebMvcConfigurer {
+
+  public void addFormatters(FormatterRegistry registry) {
+    DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+    registrar.setUseIsoFormat(true);
+    registrar.registerFormatters(registry);
+  }
+}
